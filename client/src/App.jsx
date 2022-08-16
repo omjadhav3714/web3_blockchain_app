2b6ffb7e973bc3b2a452d48c3710ee24745b14cc
user <omjadhav963@gmail.com>
2022-08-16 12:41:27 +0530

    fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Services from './components/Services';
+import Transactions from './components/Transactions';
+import Welcome from './components/Welcome';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }
